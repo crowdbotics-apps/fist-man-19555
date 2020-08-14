aa1289f8e3f0bcab4449fa17c64df36f10175670
Crowdbotics <team@crowdbotics.com>
2020-08-14 13:08:09 +0000

    Update models

--- backend/fist_man_19555/wsgi.py
+++ backend/fist_man_19555/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fist_man_19555.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fist_man_19555.settings")
 
 application = get_wsgi_application()
